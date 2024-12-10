020843050f1adfbc57b34b6c7baf19e9bcaaa030
Aishi Aratrika <aishiaratrika26@gmail.com>
2024-12-10 16:22:04 -0500

    Add 6 countries with multiple surveys

--- trend for all countries sex straification.R
+++ trend for all countries sex straification.R
@@ -1,13 +1,13 @@
+
+rm(list = ls())
+gc()
+
 library(wpp2024)
 library(rstan)
-library(V8)
-
-#first testing with 2 countries:moving the automated starting y+survey+prgm data functions for the time being
-#keeping manual data inputs for simplicity
 
 #---preparing model inputs---
 
-#male & female pop data from wpp
+# male & female pop data from wpp
 data(popM1)
 data(popF1)
 
@@ -19,9 +19,9 @@ get_popdata <- function(cnt_name, year="2020") {
            sum(wpp_f[(15 + 1):(100 + 1), year]) * 1000))
 }
 
-# pop for 2 countries
-countries <- c("Kenya", "Ghana") 
-pop <- sapply(countries, get_popdata)#2by2 matrix of male & female pop in Ken&Gha
+# pop for 6 countries
+countries <- c("Kenya", "Ghana", "Sierra Leone", "Malawi", "Madagascar", "Zimbabwe") 
+pop <- sapply(countries, get_popdata)#2by2 matrix of male & female pop in 6 countries
 
 # time specification
 start <- 2009 #same starting year for all countries
@@ -38,8 +38,6 @@ for (i in 2:length(beta_ind)) {
   yr_ind[beta_ind[i - 1]:(beta_ind[i] - 1)] <- i - 1
 }
 yr_ind[(niter - 1 / dt + 1):niter] <- length(beta_ind)
-
-
 
 # stan code
 hivst_mod <- '
@@ -98,13 +96,13 @@ data {
   int dt_yr; 
   matrix[2, n_cnt] pop;               // r = sex, c = cnt
   
-  // for prgrm data;
+  // for prgrm data
   int<lower = 1> n_hts_by_cnt[n_cnt];        // nb of years with observed HIVST data per cntry
   int<lower = 1> ind_hts[tot_hts]; // indices pgm data(max length?)
   int<lower = 1> hivst[tot_hts];   // nb of HIVST performed per year per cnty
   real se_hts[tot_hts];            // se for program data(max lenght required?)
   
-  // for survey data;
+  // for survey data
   int<lower = 1> n_svy_by_cnt[n_cnt];     // nb of surveys per country
   int<lower = 1> ind_svy[tot_svy]; // indices for survey data (max length across countries)
   int<lower = 1> num_svy[tot_svy, 2]; // numerator for survey proportions
@@ -199,14 +197,13 @@ expose_stan_functions(stanc(model_code = hivst_mod))
 hivst_stan <- stan_model(model_code = hivst_mod)
 
 #------ Survey & program data for each country ------------
-# done manually now, but I have a separate script to extract automatically from combined df of survey & prgm data
-
 cnt_data <- list(
   kenya = list(
     yr_svy = c(2012.5, 2018.5, 2022.5),
     ind_svy = (c(2012.5, 2018.5, 2022.5) - start) / dt,
     den_svy = round(cbind(c(4605, 16082, 11562), c(6350, 17880, 25725))),
     num_svy = round(cbind(c(148, 340, 1044), c(116, 436, 1242))),
+    yr_hts = c(2018,  2019,   2020,    2021,   2022,  2023),
     ind_hts = (c(2018, 2019, 2020, 2021, 2022, 2023) - start) / dt,
     hts_dat = c(197200, 400000, 595953, 630000, 342610, 617317),
     se_hts = c(197200, 400000, 595953, 630000, 342610, 617317) * 0.1
@@ -216,16 +213,56 @@ cnt_data <- list(
     ind_svy = (c(2017.5, 2022.5) - start) / dt,
     den_svy = round(cbind(c(2553, 4558), c(5575, 6250))),
     num_svy = round(cbind(c(37, 83), c(132, 151))),
+    yr_hts = c(2020,    2021,   2022,  2023),
     ind_hts = (c(2020, 2021, 2022, 2023) - start) / dt,
     hts_dat = c(20000, 1323, 235000, 140500),
     se_hts = c(20000, 1323, 235000, 140500) * 0.1
+  ),
+  sierraleone = list(
+    yr_svy = c(2017.5, 2019.5),
+    ind_svy = (c(2017.5, 2019.5) - start) / dt,
+    den_svy = round(cbind(c(2465, 2907), c(5096, 2607))),
+    num_svy = round(cbind(c(50, 62), c(165, 101))),
+    yr_hts = c(2021, 2022, 2023),
+    ind_hts = (c(2021, 2022, 2023) - start) / dt,
+    hts_dat = c(2678, 1173, 50340),
+    se_hts = c(2678, 1173, 50340) * 0.1
+    ),
+  malawi = list(
+    yr_svy = c(2015.5, 2020.5),
+    ind_svy = (c(2015.5, 2020.5) - start) / dt,
+    den_svy = round(cbind(c(2796, 5165), c(14792, 5920))),
+    num_svy = round(cbind(c(30, 406), c(136, 373))),
+    yr_hts = c(2018, 2019, 2020, 2021, 2022, 2023),
+    ind_hts = (c(2018, 2019, 2020, 2021, 2022, 2023) - start) / dt,
+    hts_dat = c(408900, 101256, 561282, 602657, 735385, 910088),
+    se_hts =  c(408900, 101256, 561282, 602657, 735385, 910088) * 0.1 
+  ),
+  madagascar = list(
+    yr_svy = c(2018.5, 2021.5),
+    ind_svy = (c(2018.5, 2021.5) - start) / dt,
+    den_svy = round(cbind(c(3055, 6178), c(5039, 6825))),
+    num_svy = round(cbind(c(35, 44), c(84, 20))),
+    yr_hts = c(2022,  2023),
+    ind_hts = (c(2022, 2023) - start) / dt,
+    hts_dat = c(2500, 2500),
+    se_hts = c(2500, 2500) * 0.1
+  ),
+  zimbabwe = list(
+    yr_svy = c(2015.5, 2019.5, 2020.5),
+    ind_svy = (c(2015.5, 2019.5, 2020.5) - start) / dt,
+    den_svy = round(cbind(c(6717, 3343, 6576), c(7964, 8104, 10058))),
+    num_svy = round(cbind(c(118, 171, 381), c(21, 447, 594))),
+    yr_hts = c(2019, 2020, 2021, 2022, 2023),
+    ind_hts = (c(2019, 2020, 2021, 2022, 2023) - start) / dt,
+    hts_dat = c(174566, 240434, 459517, 414499, 513090),
+    se_hts = c(174566, 240434, 459517, 414499, 513090) * 0.1
   )
 )
 
-#indices for countries: first testing with 2 countries w/ multiple surveys
 # list of survey years for each country
-n_cnt <- length(cnt_data) #number of countries: 2
-n_svy_by_cnt <- unlist(lapply(cnt_data, function(x) length(x$yr_svy))) #number of surveys per country
+n_cnt <- length(cnt_data) # 6 cntries
+n_svy_by_cnt <- unlist(lapply(cnt_data, function(x) length(x$yr_svy))) 
 n_hts_by_cnt <- unlist(lapply(cnt_data, function(x) length(x$ind_hts)))
 
 # survey
@@ -302,18 +339,21 @@ fit <- sampling(hivst_stan, data = data_stan, iter = 3000, chains = 4,
 # traceplots
 traceplot(fit, pars = "beta_t")
 traceplot(fit, pars = "sd_rw")
-traceplot(fit, pars = "sd_rw")
+traceplot(fit, pars = "sd_phi")
+traceplot(fit, pars = "sd_rt")
+traceplot(fit, pars = "beta_retest_overall")
 traceplot(fit, pars = "beta_retest")
 traceplot(fit, pars = "beta_male")
+traceplot(fit, pars = "phi_overall")
 traceplot(fit, pars = "phi")
 
 
-#----results with 95%CrI--------
-svy_m <- as.data.frame(rstan::summary(fit, pars = c("svy_prd_m"), probs = c(0.025, 0.25, 0.5, 0.75, 0.975))$summary)
-svy_f <- as.data.frame(rstan::summary(fit, pars = c("svy_prd_f"), probs = c(0.025, 0.25, 0.5, 0.75, 0.975))$summary)
-hts <- as.data.frame(rstan::summary(fit, pars = c("hivst_prd"), probs = c(0.025, 0.25, 0.5, 0.75, 0.975))$summary)
+# parameters
 r <- as.data.frame(rstan::summary(fit, pars = c("beta_t"), probs = c(0.025, 0.25, 0.5, 0.75, 0.975))$summary)
 exp(r$`50%`)
+
+rr_overall <- as.data.frame(rstan::summary(fit, pars = c("beta_retest_overall"), probs = c(0.025, 0.25, 0.5, 0.75, 0.975))$summary)
+exp(rr_overall$`50%`)
 rr <- as.data.frame(rstan::summary(fit, pars = c("beta_retest"), probs = c(0.025, 0.25, 0.5, 0.75, 0.975))$summary)
 exp(rr$`50%`)
 exp(rr$`2.5%`)
@@ -324,61 +364,196 @@ exp(rr_m$`50%`)
 exp(rr_m$`2.5%`)
 exp(rr_m$`97.5%`)
 
+phi_overall <- as.data.frame(rstan::summary(fit, pars = c("phi_overall"), probs = c(0.025, 0.25, 0.5, 0.75, 0.975))$summary)
+phi_overall$`50%`
 phi <- as.data.frame(rstan::summary(fit, pars = c("phi"), probs = c(0.025, 0.25, 0.5, 0.75, 0.975))$summary)
 phi$`50%`
 phi$`2.5%`
 phi$`97.5%`
 
+#----results with 95%CrI--------
 
-#------plot--------
-par(mfrow = c(1, 2), oma = c(0, 0, 2, 0), mar = c(4, 4, 1, 1))
+# male survey pred
+svy_m <- as.data.frame(rstan::summary(fit, pars = c("svy_prd_m"), probs = c(0.025, 0.25, 0.5, 0.75, 0.975))$summary)
+# medians
+medians_svy_m <- svy_m$`50%`
+# converting into matrix with niter rows and n_cnt columns
+medians_svy_m_mat <- matrix(medians_svy_m, nrow = niter, ncol = n_cnt, byrow = FALSE)
+colnames(medians_svy_m_mat) <- countries
+# 2.5% quantile estimates for svy_prd_m
+low_svy_m <- svy_m$`2.5%`
+low_svy_m_mat <- matrix(low_svy_m, nrow = niter, ncol = n_cnt, byrow = FALSE)
+colnames(low_svy_m_mat) <- countries
+# 97.5% quantile estimates for svy_prd_m
+high_svy_m <- svy_m$`97.5%`
+high_svy_m_mat <- matrix(high_svy_m, nrow = niter, ncol = n_cnt, byrow = FALSE)
+colnames(high_svy_m_mat) <- countries
 
-# survey data
-plot(svy_m$`50%` ~ time, type = "l", col = "steelblue4", lwd = 3, ylab = "Ever used HIVST(%)", ylim = c(0, 0.2))
+# female survey pred
+svy_f <- as.data.frame(rstan::summary(fit, pars = c("svy_prd_f"), probs = c(0.025, 0.25, 0.5, 0.75, 0.975))$summary)
+# medians and 95% CrI
+medians_svy_f <- svy_f$`50%`
+medians_svy_f_mat <- matrix(medians_svy_f, nrow = niter, ncol = n_cnt, byrow = FALSE)
+colnames(medians_svy_f_mat) <- countries
+
+low_svy_f <- svy_f$`2.5%`
+low_svy_f_mat <- matrix(low_svy_f, nrow = niter, ncol = n_cnt, byrow = FALSE)
+colnames(low_svy_f_mat) <- countries
+
+high_svy_f <- svy_f$`97.5%`
+high_svy_f_mat <- matrix(high_svy_f, nrow = niter, ncol = n_cnt, byrow = FALSE)
+colnames(high_svy_f_mat) <- countries
+
+# prgm data pred
+hts <- as.data.frame(rstan::summary(fit, pars = c("hivst_prd"), probs = c(0.025, 0.25, 0.5, 0.75, 0.975))$summary)
+medians_hts <- hts$`50%`
+medians_hts_mat <- matrix(medians_hts, nrow = niter, ncol = n_cnt, byrow = FALSE)
+colnames(medians_hts_mat) <- countries
+
+low_hts <- hts$`2.5%`
+low_hts_mat <- matrix(low_hts, nrow = niter, ncol = n_cnt, byrow = FALSE)
+colnames(low_hts_mat) <- countries
+
+high_hts <- hts$`97.5%`
+high_hts_mat <- matrix(high_hts, nrow = niter, ncol = n_cnt, byrow = FALSE)
+colnames(high_hts_mat) <- countries
+
+
+#------plot for individual country--------
+
+countries <- c("Kenya", "Ghana")
+
+for (country_name in tolower(countries)) {
+  # matching capitalized version
+  c_name_capital <- countries[tolower(countries) == tolower(country_name)]
+  
+  # extracting pred 
+  median_m <- medians_svy_m_mat[, c_name_capital]
+  low_m <- low_svy_m_mat[, c_name_capital]
+  high_m <- high_svy_m_mat[, c_name_capital]
+  
+  median_f <- medians_svy_f_mat[, c_name_capital]
+  low_f <- low_svy_f_mat[, c_name_capital]
+  high_f <- high_svy_f_mat[, c_name_capital]
+  
+  median_hts <- medians_hts_mat[, c_name_capital]
+  low_hts <- low_hts_mat[, c_name_capital]
+  high_hts <- high_hts_mat[, c_name_capital]
+  
+  # extracting observed survey and prgm data
+  svy_dat_c <- cnt_data[[country_name]]$svy_dat
+  lci_svy_c <- cnt_data[[country_name]]$lci_svy
+  uci_svy_c <- cnt_data[[country_name]]$uci_svy
+  ind_svy_c <- cnt_data[[country_name]]$ind_svy
+  
+  hts_dat_c <- cnt_data[[country_name]]$hts_dat
+  ind_hts_c <- cnt_data[[country_name]]$ind_hts
+  
+  par(mfrow = c(1, 2), oma = c(0, 0, 2, 0), mar = c(4, 4, 1, 1))
+  
+  # Survey data
+  plot(time, median_m, type = "l", col = "steelblue4", lwd = 3, 
+       ylab = "Ever used HIVST(%)", ylim = c(0, 0.2), xlab = "Year")
+  polygon(x = c(time, rev(time)),
+          y = c(low_m, rev(high_m)),
+          col = yarrr::transparent("steelblue4", trans.val = 0.5), border = NA)
+  polygon(x = c(time, rev(time)),
+          y = c(low_f, rev(high_f)),
+          col = yarrr::transparent("pink3", trans.val = 0.5), border = NA)
+  lines(time, median_f, col = "pink3", lwd = 3)
+  
+  points(time[ind_svy_c], svy_dat_c[, 1], pch = 16, col = "blue4")
+  points(time[ind_svy_c], svy_dat_c[, 2], pch = 16, col = "firebrick4")
+  
+  segments(x0 = time[ind_svy_c], y0 = lci_svy_c[, 1],
+           x1 = time[ind_svy_c], y1 = uci_svy_c[, 1], col = "blue4")
+  segments(x0 = time[ind_svy_c], y0 = lci_svy_c[, 2],
+           x1 = time[ind_svy_c], y1 = uci_svy_c[, 2], col = "firebrick4")
+  
+  legend("topleft", legend = c("men", "women"), col = c("steelblue4", "pink4"), lwd = 4, bty = "n")
+  
+  # Program data
+  ylim_hts <- c(0, max(high_hts, hts_dat_c, na.rm = TRUE) * 1.1)
+  plot(time, median_hts, type = "l", col = "cyan4", lwd = 3, 
+       ylab = "Number of HIVST kits distributed", ylim = ylim_hts, xlab = "Year")
+  polygon(x = c(time, rev(time)),
+          y = c(low_hts, rev(high_hts)),
+          col = yarrr::transparent("cyan4", trans.val = 0.5), border = NA)
+  
+  points(time[ind_hts_c], hts_dat_c, pch = 16, col = "goldenrod3", cex = 1.25)
+  
+  mtext(c_name_capital, outer = TRUE, side = 3, line = 1, cex = 1.5)
+}
+
+
+#------ combined fit ------------
+
+
+# --- combining ----
+extracted_fit <- extract(fit)
+ext_fit_m <- extracted_fit$svy_prd_m
+ext_fit_f <- extracted_fit$svy_prd_f
+n_sample <- dim(ext_fit_m)[1] # no of iterations (6000)
+
+#ext_fit_m[i, , ]:fixing 1st dimension at i (selecting the i-th iteration).
+#Using , (with nothing before/after) for 2nd & 3rd dim: take all elements along those dim
+#6000 rows(one for each posterior sample or iteration),150 columns (one for each time step)
+
+pool_prp <- matrix(NA, nrow = n_sample, ncol = niter) # every element starts w/ NA, otherwise error as data is missing
+for (i in 1:n_sample) {
+  ext_fit_mi <- ext_fit_m[i, , ] * data_stan$pop[1, ]
+  ext_fit_fi <- ext_fit_f[i, , ] * data_stan$pop[2, ]
+  pool_prp[i, ] <- (colSums(ext_fit_mi) + colSums(ext_fit_fi)) / sum(data_stan$pop)
+}
+
+#apply quantile function over the columns (denoted by margin=2)
+pool_lci <- apply(pool_prp, MARGIN = 2, quantile, probs = 0.025)
+pool_med <- apply(pool_prp, MARGIN = 2, quantile, probs = 0.5)
+pool_uci <- apply(pool_prp, MARGIN = 2, quantile, probs = 0.975)
+
+
+#plotting
+plot(pool_med, type = "l", lwd = 2)
+lines(pool_lci, col = "grey")
+lines(pool_uci, col = "grey")
+
+#plot
+par(mfrow = c(1,1), oma = c(2, 2, 2, 2), mar = c(4, 4, 2, 2))
+
+median_col <- "purple4"                          
+ci_shade_col <- adjustcolor("plum", alpha.f = 0.3)
+
+# Specific years you want as ticks
+custom_years <- c(2009, 2012, 2015, 2018, 2021, 2024)
+
+plot(time, pool_med, type = "n",
+     xlab = "Year",
+     ylab = "Proportion of People Having Used HIVST",
+     ylim = c(0, max(pool_uci)),
+     las = 1,   # horizontal y-axis labels
+     bty = "l",
+     xaxt = "n" # suppress default x-axis
+)
+
+# Add the confidence interval polygon
 polygon(x = c(time, rev(time)),
-        y = c(svy_m$`2.5%`, rev(svy_m$`97.5%`)),
-        col = yarrr::transparent("steelblue4", trans.val = 0.5), border = NA)
-polygon(x = c(time, rev(time)),
-        y = c(svy_f$`2.5%`, rev(svy_f$`97.5%`)),
-        col = yarrr::transparent("pink3", trans.val = 0.5), border = NA) 
-lines(svy_f$`50%` ~ time, col = "pink3", lwd = 3)
-points(svy_dat[, 1] ~ time[ind_svy], pch = 16, col = "blue4")
-points(svy_dat[, 2] ~ time[ind_svy], pch = 16, col = "firebrick4")
-segments(x0 = time[ind_svy], y0 = lci_svy[, 1],
-         x1 = time[ind_svy], y1 = uci_svy[, 1], col = "blue4")
-segments(x0 = time[ind_svy], y0 = lci_svy[, 2],
-         x1 = time[ind_svy], y1 = uci_svy[, 2], col = "firebrick4")
-legend("topleft", legend = c("men", "women"), col = c("steelblue4", "pink4"), lwd = 4, bty = "n")
+        y = c(pool_lci, rev(pool_uci)),
+        col = ci_shade_col, border = NA)
 
+# Add the median line
+lines(time, pool_med, col = median_col, lwd = 2)
 
-# program data 
-options(scipen = 999)
-plot(hts$`50%` ~ time, type = "l", col = "cyan4", lwd = 3, ylab = "Number of HIVST kits distributed",
-     ylim = c(0, 900000)) #fixed ylim for better interpretability
-polygon(x = c(time, rev(time)),
-        y = c(hts$`2.5%`, rev(hts$`97.5%`)),
-        col = yarrr::transparent("cyan4", trans.val = 0.5), border = NA)
-points(hts_dat ~ time[ind_hts], pch = 16, col = "goldenrod3", cex = 1.25)
+# Add a custom x-axis with the chosen years
+axis(1, at = custom_years, labels = custom_years)
 
-mtext("Overall trend", outer = TRUE, side = 3, line = 1, cex = 1.5)
+# Add a legend
+legend("topright",
+       legend = c("Weighted proportion for 6 countries with more than one survey", "95% CrI"),
+       lty = c(1, NA),
+       pch = c(NA, 15),
+       pt.cex = c(1, 1.5),
+       col = c(median_col, ci_shade_col),
+       bty = "n")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+title("Estimated HIV Self-Test Uptake")
 
